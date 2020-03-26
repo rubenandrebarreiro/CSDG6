b617b9256ab7621a10a992aea3e5e195aa823082
FMPI.SANTOS <fmpi.santos@campus.fct.unl.pt>
2020-03-26 20:41:15 +0000

    First Commit

---
+++ csdw1/src/main/java/fct/unl/pt/csdw1/Csdw1Application.java
@@ -0,0 +1,13 @@
+package fct.unl.pt.csdw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csdw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csdw1Application.class, args);
+	}
+
+}
---
+++ csdw1/src/test/java/fct/unl/pt/csdw1/Csdw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fct.unl.pt.csdw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csdw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
