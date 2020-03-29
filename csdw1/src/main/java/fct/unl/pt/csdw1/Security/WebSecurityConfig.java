3244fdf82bcf158c29d4fc6d06dd96507452d8ac
FMPI.SANTOS <fmpi.santos@campus.fct.unl.pt>
2020-03-29 16:03:48 +0000

    Failed attemp

--- csdw1/src/main/java/fct/unl/pt/csdw1/Security/WebSecurityConfig.java
+++ csdw1/src/main/java/fct/unl/pt/csdw1/Security/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package fct.unl.pt.csdw1.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,15 +14,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
-@EnableWebSecurity
+import javax.sql.DataSource;
+
+//@Configuration
+//@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /*@Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+        .usersByUsernameQuery("select userName,password,enabled from BankEntity where userName = ?")
+        .authoritiesByUsernameQuery("select username,authority from BankEntity where userName = ?");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/").permitAll()
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/all", true)
@@ -34,21 +44,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     };
-
+*/
 }
