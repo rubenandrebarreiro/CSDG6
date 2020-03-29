b3c889b2ab8e2260e6a4432b9b325206fd24d345
Bruno <bv.santos@campus.fct.unl.pt>
2020-03-29 17:55:09 +0100

    Repository

--- csdw1/src/main/java/fct/unl/pt/csdw1/Repositories/BankRepo.java
+++ csdw1/src/main/java/fct/unl/pt/csdw1/Repositories/BankRepo.java
@@ -1,10 +1,13 @@
 package fct.unl.pt.csdw1.Repositories;
 
 import fct.unl.pt.csdw1.Entities.BankEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface BankRepo extends CrudRepository<BankEntity, Long> {
+@Repository
+public interface BankRepo extends JpaRepository<BankEntity, Long> {
     Optional<BankEntity> findByUserName(String userName);
 }
