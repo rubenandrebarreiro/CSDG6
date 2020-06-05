7bb55dbf9f0ea0780ea31584d1d0fcdde273496b
Rúben André Barreiro <ruben.andre.letra.barreiro@gmail.com>
2020-06-05 18:00:18 +0100

    More Entities added

---
+++ csd/src/main/java/fct/unl/pt/csd/Entities/AuctionEntity.java
@@ -0,0 +1,126 @@
+package fct.unl.pt.csd.Entities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Entity;
+
+@Entity
+public class AuctionEntity {
+
+	@Id
+	@JsonProperty("id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	
+	@JsonProperty("ownerUsername")
+    private String ownerUsername;
+	
+	@JsonProperty("state")
+    private String state;
+	
+	@CollectionTable
+	@JsonProperty("bids")
+    private Map<Long, BidEntity> bids;
+
+    protected AuctionEntity() {
+    	
+    	// Empty Constructor
+    	
+    }
+    
+    @SuppressWarnings("unchecked")
+	protected AuctionEntity(String jsonObjectString)
+    		  throws JsonMappingException, JsonProcessingException {
+    	
+    	ObjectMapper objectMapper = new ObjectMapper();
+    	
+		Map<String, Object> auctionEntityMap = 
+    			objectMapper.readValue(jsonObjectString, Map.class);
+    	
+    	this.id = (Long) auctionEntityMap.get("id");
+    	
+    	this.ownerUsername = (String) auctionEntityMap.get("ownerUsername");
+    	
+    	this.state = (String) auctionEntityMap.get("state");
+    	
+    	this.bids = (HashMap<Long, BidEntity>) objectMapper.readValue("bids", HashMap.class);
+    	
+    }
+
+    public AuctionEntity(Long id, String ownerUsername) {
+        this.id = id;
+        this.ownerUsername = ownerUsername;
+        this.state = "opened";
+        this.bids = new HashMap<>();
+    }
+
+    public Long getID() {
+
+    	return this.id;
+    
+    }
+    
+    public String getOwnerUsername() {
+    	
+    	return this.ownerUsername;
+    
+    }
+    
+    public String getState() {
+    	
+    	return this.state;
+    	
+    }
+    
+    public void close() {
+    	
+    	this.state = "closed";
+    	
+    }
+
+    public Map<Long, BidEntity> getBids() {
+    
+    	return this.bids;
+    
+    }
+    
+    public void addBid(String username, Long amount) {
+    	
+    	BidEntity bid = new BidEntity( new Long( this.bids.size() ),
+    								   username, amount);
+    	
+    	this.bids.put(bid.getID(), bid);
+    	
+    }
+    
+	public Long getLastBidAmount() {
+		
+		return this.getBids().get(new Long( ( this.bids.size() - 1 ) ) )
+							 .getAmount();
+		
+	}
+    
+	public boolean validBidAmount(Long amount) {
+		
+		return amount > this.getLastBidAmount();
+		
+	}
+	
+	public String toJSONString() throws JsonProcessingException {
+		
+		return new ObjectMapper().writeValueAsString(this);
+		
+	}
+	
+}
---
+++ csd/src/main/java/fct/unl/pt/csd/Entities/BidEntity.java
@@ -0,0 +1,82 @@
+package fct.unl.pt.csd.Entities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Entity
+public class BidEntity {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
+	private Long id;
+    
+	@JsonProperty("username")
+	private String username;
+	
+	@JsonProperty("amount")
+    private Long amount;
+    
+    protected BidEntity(){};
+    
+    protected BidEntity(String jsonObjectString)
+  		  throws JsonMappingException, JsonProcessingException {
+  	
+	  	ObjectMapper objectMapper = new ObjectMapper();
+	  	
+	  	@SuppressWarnings("unchecked")
+			Map<String, Object> auctionEntityMap = 
+	  			objectMapper.readValue(jsonObjectString, Map.class);
+	  	
+	  	this.id = (Long) auctionEntityMap.get("id");
+	  	
+	  	this.username = (String) auctionEntityMap.get("username");
+	  	
+	  	this.amount = (Long) auctionEntityMap.get("amount");
+  	
+    }
+
+    public BidEntity(Long id, String username, Long amount) {
+    	
+        this.id = id;
+        this.username = username;
+        this.amount = amount;
+    
+    }
+
+    public Long getID() {
+
+    	return this.id;
+    
+    }
+
+    public String getUsername() {
+    
+    	return this.username;
+    
+    }
+    
+    public Long getAmount() {
+    
+    	return this.amount;
+    
+    }
+    
+	public String toJSONString() throws JsonProcessingException {
+		
+		return new ObjectMapper().writeValueAsString(this);
+		
+	}
+    
+}
